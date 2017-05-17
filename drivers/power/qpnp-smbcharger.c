58c9ccf9c41acd0579bf2b700a2eb480fdf7f1db
Sheersh Bansal <aggarwalace@gmail.com>
2017-05-17 12:42:12 +0530

    USB fastcharge

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -3521,6 +3521,12 @@ static void smbchg_external_power_changed(struct power_supply *psy)
 	if (usb_supply_type != POWER_SUPPLY_TYPE_USB)
 		goto  skip_current_for_non_sdp;
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge)
+	current_limit = 900;
+#endif
+	
+	
 	pr_smb(PR_MISC, "usb type = %s current_limit = %d\n",
 			usb_type_name, current_limit);
 
